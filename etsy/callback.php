1a9b4cd9504427f45cf1d0f9fb884cc9e18723d5
LY <649610857@qq.com>
2025-08-29 17:46:07 +0800

    a

---
+++ etsy/callback.php
@@ -0,0 +1,23 @@
+<?php
+use Etsy\OAuth\Client;
+
+$client_id = 'dgb4cfdpd2rg2ic73r3yaqgr';
+
+$client = new Etsy\OAuth\Client($client_id);
+
+$redirect_uri = 'https://fasteink.com/etsy/callback.php';
+$scopes = \Etsy\Utils\PermissionScopes::ALL_SCOPES;
+[$verifier, $code_challenge] = $client->generateChallengeCode();
+$nonce = $client->createNonce();
+echo '1';
+echo '$code_challenge:'.$code_challenge;
+echo '$verifier:'.$verifier;
+echo '$nonce:'.$nonce;
+exit;
+
+$url = $client->getAuthorizationUrl(
+    $redirect_uri,
+    $scopes,
+    $code_challenge,
+    $nonce
+);
