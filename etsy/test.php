4564110515f65ae06bbe3e9f0f354d1c1d50c72f
LY <649610857@qq.com>
2025-08-30 15:54:46 +0800

    a

--- etsy/test.php
+++ etsy/test.php
@@ -5,18 +5,18 @@ use Etsy\Resources\User;
 
 session_start();
 $client_id = 'dgb4cfdpd2rg2ic73r3yaqgr';
-//Access Token: 453811509.KKIeSCLcY3k1IROBtG6cWBhMORpBzFp8pJwQaiD4xrr31L_OVKeKNPb6fyn6VVk5QoHDnEMnvfrYOjd_YUKtHdLhms
-//Refresh Token: 453811509.bxEkXN7hrG1L4YOlshyBZqICV9tL_1UjvN4TYClW9h2e6-SqBDWPsdrCCPEmUBkQ3cX-fJ2oYQYp6JPalGKPgAfQfD
-$access_token = '453811509.KKIeSCLcY3k1IROBtG6cWBhMORpBzFp8pJwQaiD4xrr31L_OVKeKNPb6fyn6VVk5QoHDnEMnvfrYOjd_YUKtHdLhms';
-$refresh_token = '453811509.bxEkXN7hrG1L4YOlshyBZqICV9tL_1UjvN4TYClW9h2e6-SqBDWPsdrCCPEmUBkQ3cX-fJ2oYQYp6JPalGKPgAfQfD';
-
-$client = new \Etsy\OAuth\Client($client_id);
-$tokens = $client->refreshAccessToken($refresh_token);
-
-$access_token  = $tokens['access_token'] ?? null;
-$refresh_token = $tokens['refresh_token'] ?? null;
-echo "Access Token: " . $access_token . "<br/>";
-echo "Refresh Token: " . $refresh_token . "<br/>";
+//Access Token: 453811509.S7pIWjk2qBBQOWlSpGy5Yiu3oRvnGddT710I8PFnGyOa5qe-EKBoY296QF05bPTy-RB8GK8HWTyjKVUsJ1l0rB8jrA
+//Refresh Token: 453811509.lLi3gLS9J9Q7YSIFfEehb8eJmyLyHAvmxChJlOFFIkDHXTFvjRD9tH8CoJckux67fNE9_j9kEC8BEqLhVSQkEjvK_s
+$access_token = '453811509.S7pIWjk2qBBQOWlSpGy5Yiu3oRvnGddT710I8PFnGyOa5qe-EKBoY296QF05bPTy-RB8GK8HWTyjKVUsJ1l0rB8jrA';
+$refresh_token = '453811509.lLi3gLS9J9Q7YSIFfEehb8eJmyLyHAvmxChJlOFFIkDHXTFvjRD9tH8CoJckux67fNE9_j9kEC8BEqLhVSQkEjvK_s';
+//
+//$client = new \Etsy\OAuth\Client($client_id);
+//$tokens = $client->refreshAccessToken($refresh_token);
+//
+//$access_token  = $tokens['access_token'] ?? null;
+//$refresh_token = $tokens['refresh_token'] ?? null;
+//echo "Access Token: " . $access_token . "<br/>";
+//echo "Refresh Token: " . $refresh_token . "<br/>";
 
 $_SESSION['access_token'] = $access_token;
 
@@ -41,16 +41,21 @@ if ($user && isset($user->user_id)) {
     echo "shop_id: " . $shop_id . "<br/>";
     echo "shop_name: " . $shop_name . "<br/>";
     echo "shop_url: " . $shop_url . "<br/>";
+    //获取商品列表
 //    $listing = \Etsy\Resources\Listing::get($shop_id);
 //    echo "listing:<br/>";
 //    print_r($listing);
+    //
+    $Buyers = \Etsy\Resources\BuyerTaxonomy::all();
+    echo "Buyers:<br/>";
+    print_r($Buyers);
 
 //    $receipt = \Etsy\Resources\Receipt::get($shop_id,2600454445);
 //    echo "receipt:<br/>";
 //    print_r($receipt);
-    $address = \Etsy\Resources\UserAddress::all(['user_id'=>9127457]);
-    echo "address:<br/>";
-    print_r($address);
+//    $address = \Etsy\Resources\UserAddress::all(['user_id'=>9127457]);
+//    echo "address:<br/>";
+//    print_r($address);
 //    $address = \Etsy\Resources\UserAddress::get(1137131569365);
 //    echo "address:<br/>";
 //    print_r($address);
