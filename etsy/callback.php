392cbc61da85de91aa0741ab8e9bf6e2f2d70c12
LY <649610857@qq.com>
2025-08-30 09:52:23 +0800

    a

--- etsy/callback.php
+++ etsy/callback.php
@@ -1,23 +1,25 @@
 <?php
-use Etsy\OAuth\Client;
 
-$client_id = 'dgb4cfdpd2rg2ic73r3yaqgr';
+// 1. 获取回调参数
+$code  = isset($_GET['code']) ? $_GET['code'] : null;
+$state = isset($_GET['state']) ? $_GET['state'] : null;
+$error = isset($_GET['error']) ? $_GET['error'] : null;
 
-$client = new Etsy\OAuth\Client($client_id);
+echo $code;
+echo "<br/>";
+echo $state;
+echo "<br/>";
+echo $error;
+echo "<br/>";
 
-$redirect_uri = 'https://fasteink.com/etsy/callback.php';
-$scopes = \Etsy\Utils\PermissionScopes::ALL_SCOPES;
-[$verifier, $code_challenge] = $client->generateChallengeCode();
-$nonce = $client->createNonce();
-echo '1';
-echo '$code_challenge:'.$code_challenge;
-echo '$verifier:'.$verifier;
-echo '$nonce:'.$nonce;
-exit;
 
-$url = $client->getAuthorizationUrl(
-    $redirect_uri,
-    $scopes,
-    $code_challenge,
-    $nonce
-);
+// 2. 检查是否有错误
+if ($error) {
+    $error_description = isset($_GET['error_description']) ? $_GET['error_description'] : 'Unknown error';
+    die("Authorization failed: $error - $error_description");
+}
+
+// 3. 校验 state 防止 CSRF
+if (!isset($_SESSION['oauth2_state']) || $state !== $_SESSION['oauth2_state']) {
+    die("Invalid state parameter. Possible CSRF attack.");
+}
